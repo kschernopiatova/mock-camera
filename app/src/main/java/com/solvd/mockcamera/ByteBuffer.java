6707e0bc37df98b71c209d434edb812f9601440f
kchernopiatova <ksenia.chernopiatova@underarmour.com>
2023-12-05 13:48:43 +0300

    update ByteBuffer

--- app/src/main/java/com/solvd/mockcamera/ByteBuffer.java
+++ app/src/main/java/com/solvd/mockcamera/ByteBuffer.java
@@ -7,6 +7,7 @@ public class ByteBuffer {
     private final byte[] imgSource;
     boolean bigEndian = true;
     boolean nativeByteOrder = false;
+    int counter = 0;
 
     public ByteBuffer(byte[] bytes) {
         this.imgSource = bytes;
@@ -23,6 +24,14 @@ public class ByteBuffer {
         return this;
     }
 
+    public static ByteBuffer wrap(byte[] array) {
+        return new ByteBuffer(array);
+    }
+
+    public static ByteBuffer wrap(byte[] array, int offset, int length) {
+        return new ByteBuffer(array);
+    }
+
     public int capacity() {
         return imgSource.length;
     }
@@ -30,4 +39,64 @@ public class ByteBuffer {
     public final int remaining() {
         return imgSource.length;
     }
+
+    public ByteBuffer slice() {
+        return this;
+    }
+
+    public ByteBuffer duplicate() {
+        return this;
+    }
+
+    public ByteBuffer asReadOnlyBuffer() {
+        return this;
+    }
+
+    public byte get() {
+        return imgSource[counter++];
+    }
+
+    public ByteBuffer put(byte b) {
+        imgSource[counter++] = b;
+        return this;
+    }
+
+    public byte get(int index) {
+        return imgSource[index];
+    }
+
+    public ByteBuffer put(int index, byte b) {
+        imgSource[index] = b;
+        return this;
+    }
+
+    public ByteBuffer get(byte[] dst, int offset, int length) {
+        return get(dst);
+    }
+
+    public ByteBuffer put(ByteBuffer src) {
+        src = this;
+        return src;
+    }
+
+    public ByteBuffer put(byte[] src, int offset, int length) {
+        return this;
+    }
+
+    public final ByteBuffer put(byte[] src) {
+        return put(src, 0, src.length);
+    }
+
+    public final boolean hasArray() {
+        return imgSource.length != 0;
+    }
+
+    public final byte[] array() {
+        return imgSource;
+    }
+
+    public ByteBuffer putInt(int value) {
+        return this;
+    }
+
 }
